4fc26b65e936644cbc55d8c83da06099ed412f53
Ron Truex <tikicoder@gmail.com>
2022-02-14 14:43:08 -0700

    winget start

--- computer_config/windows/winget.ps1
+++ computer_config/windows/winget.ps1
@@ -41,3 +41,6 @@ winget install -e --id Canonical.Ubuntu
 
 write-host "Chrome installing"
 winget install -e --id Google.Chrome
+
+write-host "Slack installing"
+winget install -e --id SlackTechnologies.Slack
