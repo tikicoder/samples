f133f9f804ab5b34a8af391cb161878bee7880d6
Ron Truex <tikicoder@gmail.com>
2022-02-23 10:22:07 -0700

    updates for docker

--- computer_config/windows/WSL/general/disable_sudo_pass.sh
+++
@@ -1,3 +0,0 @@
-#!/bin.bash
-
-echo "`whoami` ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/`whoami` && sudo chmod 0440 /etc/sudoers.d/`whoami`
--- computer_config/windows/WSL/ubuntu/run_files/0_ssls.ps1
+++ computer_config/windows/WSL/ubuntu/run_files/0_ssls.ps1
@@ -3,9 +3,8 @@ $scriptPath_init = split-path -parent $MyInvocation.MyCommand.Definition
 . "$(Join-Path -Path $scriptPath_init -ChildPath "..\general\defaults.ps1")"
 
 
-$missing_root_certs_path = $(Join-Path -Path $scriptPath_init -ChildPath '..\..\missing_root_certs' )
+$missing_root_certs_path = $(Join-Path -Path $scriptPath_init -ChildPath '..\..\..\general\missing_root_certs' )
 $missing_root_certs = $(Get-Childitem -Path $missing_root_certs_path -File | Where-Object {$_.Name.ToLower().EndsWith(".crt")})
-
 
 $files_copy = Get-ChildItem "$($scriptPath_init)/*.sh" -File -Recurse
 foreach ( $file in $missing_root_certs){
