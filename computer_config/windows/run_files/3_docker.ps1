5fed1d14e0b824ee24671cfe5536b690a8acbf5e
Ron Truex <tikicoder@gmail.com>
2022-02-22 22:57:22 -0700

    updates for using rocky for docker

--- computer_config/windows/run_files/3_docker.ps1
+++ computer_config/windows/run_files/3_docker.ps1
@@ -20,8 +20,27 @@ Start-Service docker
 
 Remove-Item -Force -Confirm:$False -Recurse $tmp_dir
 
-$rocky_home_path = $(Join-Path -Path $HOME -ChildPath "AppData\Local\Packages\rocky.rc_tikicoder")
-if (-not (Test-Path -Path $(Join-Path -Path $HOME -ChildPath "AppData\Local\Packages\rocky.rc_tikicoder"))) {New-Item -ItemType Directory -Path $rocky_home_path}
+# https://docs.rockylinux.org/guides/interoperability/import_rocky_to_wsl_howto/
+# I am using Rocky as my Docker Desktop App and will install Distrod on that
+$tiki_docker_desktop_path = $(Join-Path -Path $HOME -ChildPath "AppData\Local\Packages\tiki_docker_desktop")
+if (-not (Test-Path -Path $(Join-Path -Path $HOME -ChildPath "AppData\Local\Packages\tiki_docker_desktop"))) {
+  New-Item -ItemType Directory -Path $tiki_docker_desktop_path
+  wsl --import tiki_docker_desktop $tiki_docker_desktop_path $(Join-Path -Path $scriptPath_init -ChildPath "..\..\..\docker_images\rocky_linux\rocky-container.8.4.tar.gz")
+}
 
-wsl --import rocky_rc $rocky_home_path $(Join-Path -Path $scriptPath_init -ChildPath "..\..\..\docker_images\rocky_linux\rocky-container.8.4.tar.gz")
+newUsername="tiki_docker"
 
+wsl -d tiki_docker_desktop yum update
+wsl -d tiki_docker_desktop yum install glibc-langpack-en -y
+wsl -d tiki_docker_desktop yum reinstall passwd sudo cracklib-dicts -y
+wsl -d tiki_docker_desktop adduser -G wheel $newUsername
+wsl -d tiki_docker_desktop echo -e "[user]" `>`> /etc/wsl.conf
+wsl -d tiki_docker_desktop echo -e "default=$newUsername" `>`> /etc/wsl.conf
+wsl -d tiki_docker_desktop echo -e "" `>`> /etc/wsl.conf
+wsl -d tiki_docker_desktop echo -e "[automount]" `>`> /etc/wsl.conf
+wsl -d tiki_docker_desktop echo -e "enabled = true" `>`> /etc/wsl.conf
+wsl -d tiki_docker_desktop echo -e "options = `"metadata,uid=1003,gid=1003,umask=022,fmask=11,case=off`"" `>`> /etc/wsl.conf
+
+
+
+wsl -d tiki_docker_desktop passwd $newUsername
