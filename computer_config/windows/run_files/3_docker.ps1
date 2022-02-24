b59cb7a262b468ecc330cd52800294ecf3140901
Ron Truex <tikicoder@gmail.com>
2022-02-23 21:37:29 -0700

    updates based on rocky docker setup

--- computer_config/windows/run_files/3_docker.ps1
+++ computer_config/windows/run_files/3_docker.ps1
@@ -90,6 +90,12 @@ wsl -d tiki_docker_desktop bash "$($general_defaults.tmp_directory)/disable_sudo
 wsl -d tiki_docker_desktop sudo dnf check-update
 wsl -d tiki_docker_desktop sudo dnf update -y
 
+# Enable PowerTools Repository on Rocky Linux 8
+# https://linuxways.net/red-hat/how-to-enable-powertools-repository-on-rocky-linux-8/
+wsl -d tiki_docker_desktopsudo dnf install dnf-plugins-core
+wsl -d tiki_docker_desktopsudo dnf install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+wsl -d tiki_docker_desktopsudo dnf config-manager --set-enabled powertools
+
 
 
 if (Test-Path "\\wsl$\tiki_docker_desktop$($general_defaults.tmp_directory)\distrod_install.sh"){Remove-Item -Path "\\wsl$\tiki_docker_desktop$($general_defaults.tmp_directory)\distrod_install.sh"}
