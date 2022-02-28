4eb738367642470e76ab1c855a1a514e87b764ce
Ron Truex <tikicoder@gmail.com>
2022-02-28 14:51:05 -0700

    updates based on testing

--- computer_config/windows/WSL/Ubuntu/run_files/1_init.ps1
+++ computer_config/windows/WSL/Ubuntu/run_files/1_init.ps1
@@ -6,7 +6,7 @@ if (Test-Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_di
 Copy-item -Path $(Join-Path -Path $general_defaults.root_path -ChildPath "general\wsl\config\wsl.conf") -Destination "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\wsl.conf"
 
 if (Test-Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\1_init.sh"){Remove-Item -Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\1_init.sh"}
-Copy-item -Force -Path $(Join-Path -Path $scriptPath_init -ChildPath "1_init.sh" ) -Destination "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\1_init.sh)"
+Copy-item -Force -Path $(Join-Path -Path $scriptPath_init -ChildPath "1_init.sh" ) -Destination "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\1_init.sh"
 
 
 wsl -d $general_defaults.main_distro bash "$($general_defaults.tmp_directory)/1_init.sh" "'$($general_defaults.tmp_directory)'"
