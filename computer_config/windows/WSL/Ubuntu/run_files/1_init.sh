b62764851b8e8f992004fd27d52d6a0628823aae
Ron <tikicoder@users.noreply.github.com>
2022-03-06 20:13:05 -0700

    Update 1_init.sh

--- computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
+++ computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
@@ -68,7 +68,7 @@ sudo apt-get -y install \
   lsb-release
 
 # Docker GPG
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -f --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Docker repo
 echo \
