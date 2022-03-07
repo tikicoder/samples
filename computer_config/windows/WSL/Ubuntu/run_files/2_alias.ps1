31d8c67cea946ddc4a1aa5d0a8d1b42be8c29169
Ron <tikicoder@users.noreply.github.com>
2022-03-06 20:17:13 -0700

    Update 2_alias.ps1

--- computer_config/windows/WSL/Ubuntu/run_files/2_alias.ps1
+++ computer_config/windows/WSL/Ubuntu/run_files/2_alias.ps1
@@ -3,9 +3,9 @@ $scriptPath_init = split-path -parent $MyInvocation.MyCommand.Definition
 . "$(Join-Path -Path $scriptPath_init -ChildPath "..\general\defaults.ps1")"
 
 
-if (Test-Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\1_init.sh"){Remove-Item -Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\1_init.sh"}
-Copy-item -Force -Path $(Join-Path -Path $scriptPath_init -ChildPath "1_init.sh" ) -Destination "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\1_init.sh"
+if (Test-Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\2_alias.sh"){Remove-Item -Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\2_alias.sh"}
+Copy-item -Force -Path $(Join-Path -Path $scriptPath_init -ChildPath "2_alias.sh" ) -Destination "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\2_alias.sh"
 
 
-wsl -d $general_defaults.main_distro bash "'$($general_defaults.tmp_directory)/1_init.sh'" "'$($general_defaults.tmp_directory)'"
-wsl -d $general_defaults.main_distro rm "$($general_defaults.tmp_directory)/1_init.sh"
+wsl -d $general_defaults.main_distro bash "'$($general_defaults.tmp_directory)/2_alias.sh'" "'$($general_defaults.tmp_directory)'"
+wsl -d $general_defaults.main_distro rm "$($general_defaults.tmp_directory)/2_alias.sh"
