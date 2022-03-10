265752847f8a407f3742aa88a09e6fed3d69e300
Ron Truex <tikicoder@gmail.com>
2022-03-09 19:23:56 -0700

    trying to fix docker issue

--- computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
+++ computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
@@ -68,17 +68,19 @@ sudo apt-get -y install \
   lsb-release
 
 # Docker GPG
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg -f --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg ---dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 
 # Docker repo
-echo \
-  "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 
 # Docker CLI
 sudo apt-get update
 sudo apt-get install -y docker-ce-cli
+
+# kubectl key and repo
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 
 # JQ
 # https://hub.docker.com/r/stedolan/jq
