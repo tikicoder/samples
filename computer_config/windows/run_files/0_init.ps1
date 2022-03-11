ec75588f12a44f5eec392ffd6465105b18acab2c
Ron Truex <tikicoder@gmail.com>
2022-03-11 11:39:51 -0700

    update to enable IIS

--- computer_config/windows/run_files/0_init.ps1
+++ computer_config/windows/run_files/0_init.ps1
@@ -1,6 +1,35 @@
 $scriptPath_init = split-path -parent $MyInvocation.MyCommand.Definition
 . "$(Join-Path -Path $scriptPath_init -ChildPath "..\general\defaults.ps1")"
 
+write-host "Enable IIS"
+dism /online /enable-feature `
+  /featurename:WCF-Services45 `
+  /featurename:WCF-TCP-PortSharing45 `
+  /featurename:IIS-WebServerRole `
+  /featurename:IIS-WebServer `
+  /featurename:IIS-CommonHttpFeatures `
+  /featurename:IIS-HttpErrors `
+  /featurename:IIS-HttpRedirect `
+  /featurename:IIS-ApplicationDevelopment `
+  /featurename:IIS-HealthAndDiagnostics `
+  /featurename:IIS-HttpLogging `
+  /featurename:IIS-Security `
+  /featurename:IIS-RequestFiltering `
+  /featurename:IIS-URLAuthorization `
+  /featurename:IIS-Performance `
+  /featurename:IIS-HttpCompressionDynamic `
+  /featurename:IIS-WebServerManagementTools `
+  /featurename:IIS-IIS6ManagementCompatibility `
+  /featurename:IIS-Metabase `
+  /featurename:IIS-StaticContent `
+  /featurename:IIS-DefaultDocument `
+  /featurename:IIS-DirectoryBrowsing `
+  /featurename:IIS-WebSockets `
+  /featurename:IIS-ASPNET `
+  /featurename:IIS-ASPNET45 `
+  /featurename:IIS-HttpCompressionStatic `
+  /featurename:IIS-ManagementConsole
+
 write-host "Enable Hyper-V"
 # Needed for Docker
 DISM /Online /Enable-Feature /All /FeatureName:Microsoft-Hyper-V /norestart
