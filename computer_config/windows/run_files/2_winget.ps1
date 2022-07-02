02eca7bf8858dc43e2bdb42d5b3e791d1ffc9338
Ron Truex <tikicoder@gmail.com>
2022-07-01 20:54:56 -0700

    updates based on testing

--- computer_config/windows/run_files/2_winget.ps1
+++ computer_config/windows/run_files/2_winget.ps1
@@ -7,6 +7,8 @@ function install-app-winget(){
     [string]$app_name
   )
 
+  write-host "Installing - $($app_name)"
+
   if([string]::IsNullOrWhiteSpace($($(winget upgrade -e --id $app_name).trim().tolower() -match "No installed package found2"))) {
     write-host "$app_name already installed"
     return
@@ -15,45 +17,32 @@ function install-app-winget(){
   winget install -e --id $app_name
 }
 
-write-host "7zip installing"
+
 install-app-winget -app_name "7zip.7zip"
 
-write-host "Wireshark installing"
 install-app-winget -app_name "WiresharkFoundation.Wireshark"
 
-write-host "PuTTY installing"
 install-app-winget -app_name "PuTTY.PuTTY"
 
-write-host "VisualStudioCode installing"
 install-app-winget -app_name "Microsoft.VisualStudioCode"
 & "$(Join-Path -Path $root_path_samples -ChildPath "general_programming_scripting\powershell\vsCode\main.ps1")" -isRestore $true -skip_wsl $true 
 
-write-host "PowerShell installing"
 install-app-winget -app_name "Microsoft.PowerShell"
 
-write-host "AzureStorageExplorer installing"
 install-app-winget -app_name "Microsoft.AzureStorageExplorer"
 
-write-host "MS Edge installing"
 install-app-winget -app_name "Microsoft.Edge"
 
-write-host "WindowsTerminal installing"
 install-app-winget -app_name "Microsoft.WindowsTerminal"
 
-write-host "IrfanView installing"
 install-app-winget -app_name "IrfanSkiljan.IrfanView"
 
-write-host "Atom installing"
 install-app-winget -app_name "GitHub.Atom"
 
-write-host "GIMP installing"
 install-app-winget -app_name "GIMP.GIMP"
 
-write-host "OBSStudio installing"
 install-app-winget -app_name "OBSProject.OBSStudio"
 
-write-host "Chrome installing"
 install-app-winget -app_name "Google.Chrome"
 
-write-host "Slack installing"
 install-app-winget -app_name "SlackTechnologies.Slack"
