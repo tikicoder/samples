8b4794cef9c216eb2f5d1801ee85802622d37133
Ron Truex <tikicoder@gmail.com>
2022-07-03 23:16:38 -0700

    updatesafter testing

--- computer_config/windows/run_files/3_docker_adduser.sh
+++ computer_config/windows/run_files/3_docker_adduser.sh
@@ -3,7 +3,7 @@
 local_user=$1
 local_user_groupid=$2
 local_user_id=$3
-home_path=$3
+home_path=$4
 
 if [ $(grep -ic $local_user /etc/group) -lt 1 ]; then
   sudo groupadd --gid $local_user_groupid $local_user
@@ -11,7 +11,7 @@ fi
 
 if [ $(grep -ic $local_user /etc/passwd) -lt 1 ]; then
   sudo adduser -G wheel --gid $local_user_groupid --uid $local_user_id $local_user --home $home_path
-  ln -s $home_path "/home/$local_user"
+  sudo ln -s $home_path "/home/$local_user"
 fi
 
 
