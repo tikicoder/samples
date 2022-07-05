c87039ce25928b06465b76ca351af04fc782f7a5
Ron <tikicoder@users.noreply.github.com>
2022-07-05 15:41:01 -0700

    Update 0_ssls.ps1

--- computer_config/windows/WSL/Ubuntu/run_files/0_ssls.ps1
+++ computer_config/windows/WSL/Ubuntu/run_files/0_ssls.ps1
@@ -4,13 +4,13 @@ $scriptPath_init = split-path -parent $MyInvocation.MyCommand.Definition
 
 if (Test-Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\tiki_auto_cert_update.sh"){Remove-Item -Path "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\tiki_auto_cert_update.sh"}
 Copy-item -Path $(Join-Path -Path $general_defaults.root_path -ChildPath "general\wsl\scripts\auto_cert_update.sh") -Destination "\\wsl$\$($general_defaults.main_distro)$($general_defaults.tmp_directory)\tiki_auto_cert_update.sh"
-wsl -d $($general_defaults.main_distro) -e sudo mv "$($general_defaults.tmp_directory)/tiki_auto_cert_update.sh" /usr/bin/tiki_auto_cert_update.sh
-wsl -d $($general_defaults.main_distro) -e sudo chmod 755 /usr/bin/tiki_auto_cert_update.sh
+wsl -d $($general_defaults.main_distro) sudo mv "$($general_defaults.tmp_directory)/tiki_auto_cert_update.sh" /usr/bin/tiki_auto_cert_update.sh
+wsl -d $($general_defaults.main_distro) sudo chmod 755 /usr/bin/tiki_auto_cert_update.sh
 
 wsl -d $($general_defaults.main_distro) mkdir -p "$($general_defaults.tmp_directory)/missing_certs"
 Copy-Missing-Certs -DestinationTempFolderInDistro "$($general_defaults.tmp_directory)/missing_certs" -Distro Ubuntu -DestinationSSLFolderInDistro "/usr/local/share/ca-certificates/"
 
-wsl -d $($general_defaults.main_distro) -e sudo /usr/bin/tiki_auto_cert_update.sh
+wsl -d $($general_defaults.main_distro) sudo /usr/bin/tiki_auto_cert_update.sh
 
 wsl --terminate $($general_defaults.main_distro)
 Wait-Distro-Start -Distro $general_defaults.main_distro
