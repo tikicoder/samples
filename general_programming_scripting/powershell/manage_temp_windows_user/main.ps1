05c25a3eb47e67d5033b0f39a0ebf15d681b1f87
Ron Truex <tikicoder@gmail.com>
2022-11-29 10:56:20 -0700

    script for creating /deleting temp user

---
+++ general_programming_scripting/powershell/manage_temp_windows_user/main.ps1
@@ -0,0 +1,94 @@
+
+
+# main.ps1 -isRestore $true/$false
+# code based on info from
+# https://superuser.com/questions/1080682/how-do-i-back-up-my-vs-code-settings-and-list-of-installed-extensions
+# There is a new sync setting tool, but this was done so I could easily sync settings from work and personal
+
+param (
+  [int] $AccountExpiresIn_x = 2,
+  [ValidateSet("Days", "Hours", "Minutes")]
+  [string] $AccountExpiresInType = "Hours",
+  [string] $AccountUserName = "temp_local_user",
+  [bool] $IsAdmin = $false,
+  [bool] $DeleteUser = $false,
+  [bool] $DeleteUserConfirm = $true
+)
+
+function Get-RandomPassword {
+  param (
+      [Parameter(Mandatory)]
+      [int] $length,
+      [int] $amountOfNonAlphanumeric = 1
+  )
+  $letters = "abcdefghijklmnopqrstuvwxyz"
+  $nonalpha = "#$=%^&*"
+
+  $password = ""
+
+  while ($password.Length -lt $length) {
+    $password_letter = $letters[$(Get-Random -Maximum ($letters.length-1) -Minimum 0)]
+    if ( ($(Get-Random -Maximum 100 -Minimum 1) % $(Get-Random -Maximum 5 -Minimum 2) ) -eq 0 ){
+      $password_letter = $password_letter.ToString().ToUpper() 
+    }
+
+    $password = $password + $password_letter  
+    
+    if ( (($password.Length) -eq ($length - $amountOfNonAlphanumeric) -and $amountOfNonAlphanumeric -gt 0) -or ($amountOfNonAlphanumeric -gt 0 -and ($(Get-Random -Maximum 100 -Minimum 1) % $(Get-Random -Maximum 5 -Minimum 2) ) -eq 0 )){
+      if (($password.Length) -eq ($length - $amountOfNonAlphanumeric) -and $amountOfNonAlphanumeric -gt 0 ){
+        while ($password.Length -lt $length) {
+          $amountOfNonAlphanumeric = $amountOfNonAlphanumeric - 1
+          $password = $password + $nonalpha[$(Get-Random -Maximum ($nonalpha.length-1) -Minimum 0)]  
+        }
+      }
+      else {
+        $amountOfNonAlphanumeric = $amountOfNonAlphanumeric - 1
+        $password = $password + $nonalpha[$(Get-Random -Maximum ($nonalpha.length-1) -Minimum 0)]  
+      }
+    }
+    
+  }
+
+  return $password
+  
+}
+
+if ($DeleteUser) {
+  if ((Get-LocalUser | Where-Object {$_.Name -ieq $AccountUserName} | Measure-Object).Count -lt 1){
+    Write-Host "$($AccountUserName) does not exist, skipping Delete"
+    exit
+  }
+  Write-Host "This will Delete the following user. This Cannot be reversed."
+  Remove-LocalUser -Name $AccountUserName -Confirm:$DeleteUserConfirm
+  exit
+}
+
+if ((Get-LocalUser | Where-Object {$_.Name -ieq $AccountUserName} | Measure-Object).Count -gt 0){
+  
+  Write-Host "$($AccountUserName) already Exists"
+  exit
+}
+
+$TimeSpanParmeters = @{
+  $AccountExpiresInType = $AccountExpiresIn_x
+}
+$AccountExpires = (Get-Date) + (New-TimeSpan @TimeSpanParmeters)
+
+
+$UserPassword = ConvertTo-SecureString -AsPlainText -String (Get-RandomPassword -length 16 -amountOfNonAlphanumeric 2)
+New-LocalUser `
+   -AccountExpires $AccountExpires `
+   -Description "Temp User, expires: $($AccountExpires.ToString("yyyyMMddTHH:mm:ss"))" `
+   -Name $AccountUserName `
+   -Password $UserPassword `
+   -Confirm:$false
+
+if ( $IsAdmin ){
+  Add-LocalGroupMember -Group “Administrators” -Member $AccountUserName
+}
+
+Write-Host "Current DateTime: $(Get-Date)"
+Write-Host "Is Admin User: $($IsAdmin)"
+Write-Host "User Information:"
+$AccountUserName
+ConvertFrom-SecureString -AsPlainText -SecureString $UserPassword
