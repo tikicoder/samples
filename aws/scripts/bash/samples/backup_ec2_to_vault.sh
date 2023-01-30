7e662a43007a1ee545ec418ff469271d6a448955
Ron <tikicoder@users.noreply.github.com>
2023-01-30 11:30:50 -0700

    Update backup_ec2_to_vault.sh

--- aws/scripts/bash/samples/backup_ec2_to_vault.sh
+++ aws/scripts/bash/samples/backup_ec2_to_vault.sh
@@ -1,7 +1,12 @@
 instance_region=$(curl http://169.254.169.254/latest/meta-data/placement/region)
 instance_id=$(curl http://169.254.169.254/latest/meta-data/instance-id)
+default_backup_name="default"
 
-backup_vault_name=$(aws backup list-backup-vaults --region "${instance_region}" | grep "\BackupVaultName\":" | head -1 | awk '{ print $2 }' | tr -d "," | tr -d '"')
+backup_vault_name=$(aws backup list-backup-vaults --region "${instance_region}" | grep -i "${default_backup_name}" | grep "\BackupVaultName\":" | head -1 | awk '{ print $2 }' | tr -d "," | tr -d '"')
+if [ "${backup_vault_name}" -z ]; then
+ backup_vault_name=$(aws backup list-backup-vaults --region "${instance_region}" | grep "\BackupVaultName\":" | head -1 | awk '{ print $2 }' | tr -d "," | tr -d '"')
+fi
+
 account_id=$(aws sts get-caller-identity | grep "\Account\":" | awk '{ print $2 }' | tr -d "," | tr -d '"' )
 instance_role_arn="arn:aws:iam::${account_id}:role/service-role/AWSBackupDefaultServiceRole"
 
