919d762dcdd4c7b8f46303967be4397bfaf1c559
Ron <tikicoder@users.noreply.github.com>
2023-03-08 12:59:55 -0700

    Update 1_init.sh
    now there is a package manager way to instal k8s lens

--- computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
+++ computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
@@ -281,14 +281,19 @@ echo 'export PATH="${KREW_ROOT:-$HOME/.krew}/bin:$PATH"' >> "${user_bash_file}"
 echo "" >> "${user_bash_file}"
 
 echo "k8s lens"
-#K8s Lens
-k8s_lens_version="2022.12.121519-latest"
-mkdir /tmp/k8s_lens
-pushd /tmp/k8s_lens
-wget "https://downloads.k8slens.dev/ide/Lens-${k8s_lens_version}.amd64.deb" -O k8s.lens.amd64.deb
-sudo apt install -y ./k8s.lens.amd64.deb
-popd
-rm -Rf /tmp/k8s_lens
+# K8s Lens
+# k8s_lens_version="2022.12.121519-latest"
+# mkdir /tmp/k8s_lens
+# pushd /tmp/k8s_lens
+# wget "https://downloads.k8slens.dev/ide/Lens-${k8s_lens_version}.amd64.deb" -O k8s.lens.amd64.deb
+# sudo apt install -y ./k8s.lens.amd64.deb
+# popd
+# rm -Rf /tmp/k8s_lens
+curl -fsSL https://downloads.k8slens.dev/keys/gpg | gpg --dearmor | sudo tee /usr/share/keyrings/lens-archive-keyring.gpg > /dev/null
+echo "deb [arch=amd64 signed-by=/usr/share/keyrings/lens-archive-keyring.gpg] https://downloads.k8slens.dev/apt/debian stable main" | sudo tee /etc/apt/sources.list.d/lens.list > /dev/null
+sudo apt update
+sudo apt install lens
+
 
 # # GCP CLI
 # # https://cloud.google.com/sdk/docs/downloads-docker
