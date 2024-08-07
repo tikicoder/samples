c7f4d7d3598f5a6390d4e6f46488c7b7932b1770
Ron <tikicoder@gmail.com>
2024-08-07 09:41:42 -0700

    updates for azure cli

--- computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
+++ computer_config/windows/WSL/Ubuntu/run_files/1_init.sh
@@ -239,11 +239,32 @@ az bicep upgrade
 # Without Prompt
 # az config set extension.use_dynamic_install=yes_without_prompt
 
+pwsh -c {
+  Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    $completion_file = New-TemporaryFile
+    $env:ARGCOMPLETE_USE_TEMPFILES = 1
+    $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+    $env:COMP_LINE = $wordToComplete
+    $env:COMP_POINT = $cursorPosition
+    $env:_ARGCOMPLETE = 1
+    $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+    $env:_ARGCOMPLETE_IFS = "`n"
+    $env:_ARGCOMPLETE_SHELL = 'powershell'
+    az 2>&1 | Out-Null
+    Get-Content $completion_file | Sort-Object | ForEach-Object {
+      [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+    }
+    Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+  }
+}
+
 echo "Azure Powershell"
 pwsh -Command Install-Module -Name Az -Scope CurrentUser -Repository PSGallery -Force
 
 pwsh -Command Install-Module -Name PSRule -Scope CurrentUser -Repository PSGallery -Force
 pwsh -Command "Find-Module -Repository PSGallery -Name 'PSRule.Rules.*' | ForEach-Object{ Install-Module -Name \$_.Name -Scope CurrentUser -Repository PSGallery -Force}"
+
 
 echo "aws cli"
 # AWS CLI
--- computer_config/windows/run_files/2_winget.ps1
+++ computer_config/windows/run_files/2_winget.ps1
@@ -95,6 +95,25 @@ install-app-winget -app_name "SlackTechnologies.Slack"
 install-app-winget -app_name "Postman.Postman"
 
 install-app-winget -app_name "Microsoft.AzureCLI"
+pwsh -c {
+  Register-ArgumentCompleter -Native -CommandName az -ScriptBlock {
+    param($commandName, $wordToComplete, $cursorPosition)
+    $completion_file = New-TemporaryFile
+    $env:ARGCOMPLETE_USE_TEMPFILES = 1
+    $env:_ARGCOMPLETE_STDOUT_FILENAME = $completion_file
+    $env:COMP_LINE = $wordToComplete
+    $env:COMP_POINT = $cursorPosition
+    $env:_ARGCOMPLETE = 1
+    $env:_ARGCOMPLETE_SUPPRESS_SPACE = 0
+    $env:_ARGCOMPLETE_IFS = "`n"
+    $env:_ARGCOMPLETE_SHELL = 'powershell'
+    az 2>&1 | Out-Null
+    Get-Content $completion_file | Sort-Object | ForEach-Object {
+      [System.Management.Automation.CompletionResult]::new($_, $_, "ParameterValue", $_)
+    }
+    Remove-Item $completion_file, Env:\_ARGCOMPLETE_STDOUT_FILENAME, Env:\ARGCOMPLETE_USE_TEMPFILES, Env:\COMP_LINE, Env:\COMP_POINT, Env:\_ARGCOMPLETE, Env:\_ARGCOMPLETE_SUPPRESS_SPACE, Env:\_ARGCOMPLETE_IFS, Env:\_ARGCOMPLETE_SHELL
+  }
+}
 
 install-app-winget -app_name "Microsoft.AzureStorageExplorer"
 
