84cd507508f66e7bac1c39369b27c014cc422686
Ron <tikicoder@gmail.com>
2025-02-24 14:34:56 -0700

    download github improvement

--- computer_config/windows/general/wsl/scripts/download_release_github.sh
+++ computer_config/windows/general/wsl/scripts/download_release_github.sh
@@ -4,15 +4,24 @@ repo_owner=$1
 repo=$2
 file_name=$3
 version="latest"
+checksum_file=""
 if [ $# -gt 3 ]; then
   version=$4
   version=$(echo "${version}" | tr '[:upper:]' '[:lower:]')
 fi
+if [ $# -gt 4 ]; then
+  checksum_file=$5
+fi
 
-mkdir -p /tmp/github-release/
+save_location="/tmp/github-release/${repo}"
+mkdir -p $save_location
 
 if [ $version == "latest" ]; then
   version=$(curl -s https://api.github.com/repos/${repo_owner}/${repo}/releases/latest | jq -r '.tag_name')
 fi
 
-wget "https://github.com/${repo_owner}/${repo}/releases/download/${version}/${file_name}" -q -O /tmp/github-release/${file_name}
+wget https://github.com/${repo_owner}/${repo}/releases/download/${version}/${file_name} -q -O "${save_location}/${file_name}"
+
+if [ ! -z "${checksum_file}" ]; then
+  wget https://github.com/${repo_owner}/${repo}/releases/download/${version}/${checksum_file} -q -O "${save_location}/${checksum_file}"
+fi
