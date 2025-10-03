bdb0abc3b1a76707c7e33906bbff8c4ef4b086fd
Ron <tikicoder@users.noreply.github.com>
2025-10-03 16:46:26 -0700

    Update 4_apps_with_gui.sh

--- computer_config/windows/WSL/Ubuntu/run_files/4_apps_with_gui.sh
+++ computer_config/windows/WSL/Ubuntu/run_files/4_apps_with_gui.sh
@@ -37,7 +37,7 @@ pushd chrome
 
 # https://github.com/microsoft/wslg#install-and-run-gui-apps
 sudo wget -O chrome.deb https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo apt install -f -y ./google-chrome-stable_current_amd64.deb
+sudo apt install -f -y ./chrome.deb
 
 
 popd
