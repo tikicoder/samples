5294655881e359abf797fe60f6198e00c97887a7
Ron <tikicoder@users.noreply.github.com>
2025-10-03 13:17:45 -0700

    Create runscript_default.ps1

---
+++ computer_config/windows/general/wsl/runscript_default.ps1
@@ -0,0 +1,6 @@
+Ensure-TempDirectory-Populated `
+  -Distro $general_defaults.main_distro `
+  -rootPath $general_defaults.root_path `
+  -wslSharePath $general_defaults.wsl_share_path `
+  -tmpDirectoryPath $general_defaults.tmp_directory `
+  -scriptPathInitMainset $scriptPath_init_mainset
