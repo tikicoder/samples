ddd4ca38a1d6c35d776fe540d3e8681f3c0c9647
Ron <tikicoder@users.noreply.github.com>
2025-10-03 18:44:03 -0700

    Update download_release_github.sh

--- computer_config/windows/general/wsl/scripts/download_release_github.sh
+++ computer_config/windows/general/wsl/scripts/download_release_github.sh
@@ -17,8 +17,9 @@ save_location="/tmp/github-release/${repo}"
 mkdir -p $save_location
 
 if [ $version == "latest" ]; then
-  download=1                                                                                                              while [ $download -eq 1 ]; do
-    curl --retry 5 --max-time 300 -s https://api.github.com/repos/${repo_owner}/${repo}/releases/latest > $save_location/latest.json
+  download=1                                                                                                              
+  while [ $download -eq 1 ]; do
+   curl --retry 5 --max-time 300 -s https://api.github.com/repos/${repo_owner}/${repo}/releases/latest > $save_location/latest.json
    version=$(cat $save_location/latest.json | jq -r '.tag_name' 2>/dev/null)
    if [ $? -eq 0 ]; then
      download=0
